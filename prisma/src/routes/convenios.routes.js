0ef1c6a1af0494a8be1bce534775e164749473d9
AndreYuli <126515557+AndreYuli@users.noreply.github.com>
2025-09-15 16:42:01 -0500

    Update Prisma

--- prisma/src/routes/convenios.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { crearConvenio } from "../controllers/convenios.controller.js";
-
-const router = Router();
-
-router.post("/", crearConvenio);
-
-export default router;
